6ffbf5435cb271e1b5bcf991b0eca4ab47f57a59
Moral Mohammad Selim <selimita2025@gmail.com>
2022-12-22 13:48:18 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+;(function($){
+    $(document).ready(function(){
+        $("#justifiedgallery").justifiedGallery({
+            rowHeight:200,
+            lastRow : 'nojustify',
+            captions:false
+        });
+    });
+})(jQuery);
